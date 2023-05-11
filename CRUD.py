7f313f51e1c40aebbdba6d464202dffd2ebfb2c8
Carlos Tavares <carlostavares2350@gmail.com>
2023-05-11 18:10:51 -0300

    Adicionando primeiro dado no banco de dados

--- CRUD.py
+++ CRUD.py
@@ -9,6 +9,13 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 #CRUD
+id_cpf_cliente = "111.222.333-44"
+nome_cliente =  "Carlos Tavares"
+nome_porduto = "Pizza"
+valor_venda = 70.50
+
+comando = f'INSERT INTO tb_vendas (id_cpf_cliente,nome_cliente,nome_porduto,valor_venda) VALUES ("{id_cpf_cliente}","{nome_cliente}",' \
+f'"{nome_porduto}",{valor_venda})'
 
 cursor.execute(comando)
 conexao.commit() #adita o banco de dados
